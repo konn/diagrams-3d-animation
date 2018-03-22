33433ea1abaad7b6cbb238c3893649d2e6348a1d
Hiromi ISHII <konn.jinro@gmail.com>
2018-03-23 01:44:56 +0900

    perspective

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,23 @@
+{-# LANGUAGE TupleSections #-}
 module Main where
+import Diagrams.Backend.Cairo
+import Diagrams.Backend.Cairo.CmdLine
+import Diagrams.Backend.CmdLine
+import Diagrams.Prelude
+import Diagrams.ThreeD.Animation
+
+dataSet :: [(String, (Double, Double, Double))]
+dataSet = [("A", (1,-1,0)), ("B", (0, 1.5, 1)), ("C", (2, 0,-2))]
+
+anime :: Animation Cairo V2 Double
+anime = scatter3DWithLabel dataSet
+
+toDias :: Duration Rational -> Animation b V2 Double -> [QDiagram b V2 Double Any]
+toDias dur = simulate 2 . stretchTo dur
 
 main :: IO ()
-main = undefined
+main = do
+  let dOpt = DiagramOpts (Just 300) Nothing "anime.gif"
+      gOpts = GifOpts False True Nothing
+  gifRender (dOpt, gOpts) $ map (,10) $ toDias 30 anime
+
--- src/Diagrams/ThreeD/Animation.hs
+++ src/Diagrams/ThreeD/Animation.hs
@@ -1,5 +1,5 @@
-{-# LANGUAGE FlexibleContexts, GADTs, NoMonomorphismRestriction #-}
-{-# LANGUAGE PartialTypeSignatures, TypeFamilies                #-}
+{-# LANGUAGE FlexibleContexts, GADTs, NoMonomorphismRestriction   #-}
+{-# LANGUAGE PartialTypeSignatures, PatternSynonyms, TypeFamilies #-}
 {-# OPTIONS_GHC -Wno-partial-type-signatures #-}
 module Diagrams.ThreeD.Animation
   ( isometricFilm
@@ -7,18 +7,33 @@ module Diagrams.ThreeD.Animation
   , animatedPlot
   , scatter3DWithLabel
   ) where
+import Debug.Trace
 import Diagrams.LinearMap
-import Diagrams.Prelude
+import Diagrams.Prelude           hiding (trace)
 import Diagrams.ThreeD.Projection
+import Linear                     ((!*!))
 import Linear.Epsilon             (Epsilon)
+
+tr :: Show b => String -> b -> b
+tr lab a = trace (lab <> show a) $! a
 
 type ActiveCamera n = Active (P3 n, Direction V3 n)
 
-isometricFilm :: (Floating n, Epsilon n)
-              => P3 n -> Active (P3 n, Direction V3 n) -> P3 n -> Active (P2 n)
+isometricFilm :: (Floating n, Epsilon n, _)
+              => P3 n -> Active (P3 n, Direction V3 n) -> a -> Active b
 isometricFilm orig cam space =
-  let camera = uncurry (`lookingAt` orig) <$> cam
-  in flip amap space <$> camera
+  cam <&> \(e, d) ->
+    let proj = lookingAt e orig d
+    in amap proj space # moveOriginTo (amap proj orig)
+
+perspectiveFilm :: (Floating n, Epsilon n, _)
+                => Angle n -> P3 n -> Active (P3 n, Direction V3 n) -> a -> Active b
+perspectiveFilm vision orig cam space =
+  cam <&> \(P e, d) ->
+   let standPoint = lookAt e zero (fromDir d)
+       proj = m44Deformation $
+              perspective (vision ^. rad) 0.8 1 3 !*! standPoint
+   in deform proj space # moveOriginTo (deform proj orig)
 
 -- | Camera looking at origin, rotating around the z-axis with specified
 --   radius and height. It rotates in unit-time.
@@ -31,31 +46,59 @@ rotatingAroundZ r h =
   let rt = fromRational $ fromTime t
       (x,y) = (r * cos (rt * 2 * pi), r * sin (rt * 2* pi))
       vec   = V3 x y h
-  in (P vec , direction (- vec))
+  in (P vec , direction unitZ)
 
-uncurry3 :: (t1 -> t2 -> t3 -> t4) -> (t1, t2, t3) -> t4
-uncurry3 f (a, b, c) = f a b c
-
-animatedPlot :: (Ord n, Semigroup m, Floating n, Epsilon n, Traversable t)
+animatedPlot :: ( Ord n, Floating n, Epsilon n, Traversable t, _)
              => ActiveCamera n
-             -> (a -> QDiagram b V2 n m)
+             -> (a -> QDiagram b V2 n Any)
              -> t (a, (n,n,n))
-             -> QAnimation b V2 n m
+             -> QAnimation b V2 n Any
 animatedPlot cam r =
   fmap (foldMap (\(a, pos) -> r a # moveTo pos)) .
-  traverse (uncurry $ \a -> fmap ((,) a) . isometricFilm origin cam . P . uncurry3 V3)
+  traverse (uncurry $ \a -> fmap ((,) a) . perspectiveFilm (pi/3 @@ rad) origin cam . p3)
 
 text' :: _ => Double -> String -> Diagram b
-text' n str = text str # fontSizeL n <> strutY (n * 1.3)
+text' n str = text str # fontSizeG n <> strutY (n * 1.3)
 
-renderScatter3D :: _ => String -> Diagram b
-renderScatter3D lab =
-  mconcat [ text' 5 lab
-          , circle 5 # fc red
-          ]
+pattern P3 :: x -> x -> x -> P3 x
+pattern P3 x y z = P (V3 x y z)
 
-scatter3DWithLabel :: (Foldable t, _) => t (String, (Double, Double, Double))
+pattern P2 :: x -> x -> P2 x
+pattern P2 x y = P (V2 x y)
+
+axes :: (Floating n, Ord n) => n -> Path V3 n
+axes d = mconcat [ origin ~~ P3 d 0 0
+                 , origin ~~ P3 0 d 0
+                 , origin ~~ P3 0 0 d
+                 ]
+
+renderScatter3D :: _ => Double -> String -> Diagram b
+renderScatter3D n lab = mconcat
+  [
+    text' n lab # moveTo (P2 (3*n/2) (3*n/2))
+  , circle (n/2) # fc red # lcA transparent
+  ]
+
+pattern Exactly :: a -> Option a
+pattern Exactly a = Option (Just a)
+
+pattern None :: Option a
+pattern None = Option Nothing
+
+scatter3DWithLabel :: (Foldable t, _)
+                   => t (String, (Double, Double, Double))
                    -> QAnimation b V2 Double Any
-scatter3DWithLabel lpts =
-  let r = maximum $ fmap (\(_,(x,y,z)) -> sqrt (x*x + y*y + z*z)) lpts
-  in animatedPlot (rotatingAroundZ (r * 1.5) (r * 1.5)) renderScatter3D lpts
+scatter3DWithLabel ls0 =
+  let (rMax, lpts) =
+        foldr (\(l,(x,y,z)) (u, ls) ->
+                  (u `max` maximum (map abs [x,y,z])
+                  , (l, (10 * x / rMax, 10 * y / rMax, 10 * z / rMax) ) : ls)
+              )
+          (1, []) ls0
+      cam = rotatingAroundZ 10 10
+      background = square 5 # fc white # lc white
+  in foldr1 atop <$>
+     sequenceA [ animatedPlot cam (renderScatter3D 0.1) lpts
+               , lw 1 . fcA transparent . lc black . stroke <$> perspectiveFilm (pi/3 @@ rad) origin cam (axes 10)
+               , background
+               ]
